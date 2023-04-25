9b4cfd548913e4da5edd0549d71f0f46ea560535
Faviobk <barberisfavio94@gmail.com>
2023-04-25 08:43:22 -0300

    Initial commit

---
+++ imdb_scraper.py
@@ -0,0 +1,37 @@
+import requests
+from requests import get
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import chardet
+
+# web to scrap
+url = "https://www.imdb.com/search/title/?groups=top_1000&ref_=adv_prv"
+headers = {"Accept-Language": "en-US, en;q=0.5"}
+results = requests.get(url, headers=headers)
+
+# Detect encoding
+encoding = chardet.detect(results.content)['encoding']
+
+# if not detect, try utf-8
+if encoding is None:
+    encoding = 'utf-8'
+
+# Decode the response of web page
+soup = BeautifulSoup(results.content.decode(encoding), "html.parser")
+# visualize the response
+# print(soup.prettify())
+
+# empty lists to store data
+titles = []
+years = []
+time = []
+imdb_ratings = []
+metascores = []
+votes = []
+us_gross = []
+
+# movies are in the lister-item mode-advanced div
+movie_div = soup.find_all('div', class_='lister-item mode-advanced')
+
+# loop through each container
